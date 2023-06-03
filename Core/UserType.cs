7394de66c2639ac2722e34255ae5f6a1c33ff8ea
Jani Giannoudis <jani@giannoudis.ch>
2023-06-03 12:47:18 +0200

    added user type updated version to 0.5.0-pre-230603-2

---
+++ Core/UserType.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace PayrollEngine;
+
+/// <summary>The type of the user</summary>
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum UserType
+{
+    /// <summary>Regular user</summary>
+    Regular = 0,
+
+    /// <summary>User with employee self service</summary>
+    Employee = 1,
+
+    /// <summary>User who can manage all users</summary>
+    Supervisor = 2
+}
