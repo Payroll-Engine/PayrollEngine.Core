1a5f3fb4aa43fbb039ea02a71b6c42123887d372
Jani Giannoudis <jani@giannoudis.ch>
2023-06-06 20:34:50 +0200

    added case field aggregation type updated version to 0.5.0-pre-230606-3

---
+++ Core/CaseFieldAggregationType.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace PayrollEngine;
+
+/// <summary>Aggregation type for a period case field <see cref="CaseFieldTimeType.Period"/></summary>
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum CaseFieldAggregationType
+{
+    /// <summary>Summary of period values</summary>
+    Summary = 0,
+
+    /// <summary>The last period value</summary>
+    Last = 1,
+
+    /// <summary>The first period value</summary>
+    First = 2
+}
