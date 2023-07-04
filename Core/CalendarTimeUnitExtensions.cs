a2cfbc2e03061cbf8f8c93e69c0f3f0cbcc07181
Jani Giannoudis <jani@giannoudis.ch>
2023-07-04 08:17:37 +0200

    new calendar time unit extensio: period count updated version to 0.5.0-pre-230703-2

--- Core/CalendarTimeUnitExtensions.cs
+++ Core/CalendarTimeUnitExtensions.cs
@@ -1,8 +1,32 @@
-﻿namespace PayrollEngine;
+﻿
+namespace PayrollEngine;
 
 /// <summary>Extensions for <see cref="CalendarTimeUnit"/></summary>
 public static class CalendarTimeUnitExtensions
 {
+    /// <summary>Get the count of periods within the cycle</summary>
+    /// <param name="cycleUnit">The payroll cycle unit</param>
+    /// <param name="periodUnit">The payroll period unit</param>
+    /// <returns>True for a valid cycle/period combination</returns>
+    public static int PeriodCount(this CalendarTimeUnit cycleUnit, CalendarTimeUnit periodUnit)
+    {
+        // period unit must be larger as the cycle unit
+        if (periodUnit < cycleUnit)
+        {
+            return 0;
+        }
+
+        var periodValue = (decimal)periodUnit;
+        var cycleValue = (decimal)cycleUnit;
+        if (periodValue == 0 || cycleValue == 0)
+        {
+            return 0;
+        }
+
+        // test division
+        return decimal.ToInt32(periodValue / cycleValue);
+    }
+
     /// <summary>Test for valid time unit combination</summary>
     /// <param name="cycleUnit">The payroll cycle unit</param>
     /// <param name="periodUnit">The payroll period unit</param>
@@ -22,7 +46,7 @@ public static class CalendarTimeUnitExtensions
             return false;
         }
 
-        // test division
+        // remainder should be zero
         return periodValue % cycleValue == 0m;
     }
 }
