583e71d571f729225ca90bd98dd167e124fe12e9
Jani Giannoudis <jani@giannoudis.ch>
2023-07-19 09:44:40 +0200

    added password change request updated version to 0.5.0-230719

---
+++ Core/PasswordChangeRequest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace PayrollEngine;
+
+/// <summary>
+/// Password change request
+/// Use cases:
+/// - set initial password: new=required, existing=ignored
+/// - change existing password: new=required, existing=required
+/// - reset password: new=null, existing=required
+/// </summary>
+public class PasswordChangeRequest : IEquatable<PasswordChangeRequest>
+{
+    /// <summary>
+    /// The new password
+    /// </summary>
+    public string NewPassword { get; set; }
+
+    /// <summary>
+    /// The existing password
+    /// </summary>
+    public string ExistingPassword { get; set; }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PasswordChangeRequest"/> class
+    /// </summary>
+    public PasswordChangeRequest()
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PasswordChangeRequest"/> class
+    /// </summary>
+    /// <param name="copySource">The copy source.</param>
+    public PasswordChangeRequest(PasswordChangeRequest copySource)
+    {
+        CopyTool.CopyProperties(copySource, this);
+    }
+
+    /// <summary>Compare two objects</summary>
+    /// <param name="compare">The object to compare with this</param>
+    /// <returns>True for objects with the same data</returns>
+    public bool Equals(PasswordChangeRequest compare) =>
+        CompareTool.EqualProperties(this, compare);
+}
